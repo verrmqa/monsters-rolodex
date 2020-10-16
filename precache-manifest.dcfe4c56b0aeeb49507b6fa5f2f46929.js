8ac66ca1d0f197a07cd71766a2225e1cb370f5c7
verrmqa <andreyqq@gmail.com>
2020-10-16 17:27:32 +0400

    Updates

---
+++ precache-manifest.dcfe4c56b0aeeb49507b6fa5f2f46929.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdb03077b43bb349e74d6989123cb239",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "703d42668d6a72ed48de",
+    "url": "/monsters-rolodex/static/css/main.5e9bd11b.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "703d42668d6a72ed48de",
+    "url": "/monsters-rolodex/static/js/main.5177648c.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
